301a68a2cb345e6e2821cfb321b7d1bd23abafcd
srikar-1997 <thalladasrikar7@msitprogram.net>
2020-05-28 19:37:19 +0530

    final complete

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ assert(isPrime(5), " test case 1 pass - isPrime(5)");
 assert(isPrime(7), " test case 2 pass - isPrime(7)");
 assert(isPrime(11), " test case 3 pass - isPrime(11)");
 assert(isPrime(13), " test case 4 pass - isPrime(17)");
-assert(isPrime(20), " test case 5 pass - isPrime(20)");
+assert(isPrime(20) === false, " test case 5 pass - isPrime(20)");
 
 
 console.log("all tests passed")
