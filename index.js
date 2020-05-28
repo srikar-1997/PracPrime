3bb27fedfe371cfa30ec7f74f8f63addbcafd85c
srikar-1997 <thalladasrikar7@msitprogram.net>
2020-05-28 19:55:10 +0530

    last check

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var assert = require('assert')
 
 assert(isPrime(5), " test case 1 pass - isPrime(5)");
 assert(isPrime(7), " test case 2 pass - isPrime(7)");
-assert(isPrime(20), " test case 3 pass - isPrime(11)");
+assert(isPrime(11), " test case 3 pass - isPrime(11)");
 assert(isPrime(13), " test case 4 pass - isPrime(17)");
 assert(isPrime(20) === false, " test case 5 pass - isPrime(20)");
 
